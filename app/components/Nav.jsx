8467612289c38b2501fd27f46ca70d8937b7d897
Fahd <fwahab9@gmail.com>
2017-01-31 19:43:27 -0800

    Added foundation configuration and nav bar css

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,15 +1,46 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = () => {
-  return (
-    <div>
-      <h2>Nav Bar</h2>
-      <IndexLink activeClassName="active" activeStyle={{fontWeight: 'bold'}} to="/">Get Weather</IndexLink>
-      <Link activeClassName="active" activeStyle={{fontWeight: 'bold'}} to="/about">About</Link>
-      <Link activeClassName="active" activeStyle={{fontWeight: 'bold'}} to="/examples">Examples</Link>
+var Nav = React.createClass({
+  onSearch: function(e) {
+    e.preventDefault();
+    alert('wiating');
+  },
+
+  render: function() {
+    return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            React Weather App
+          </li>
+          <li>
+            <IndexLink activeClassName="active" activeStyle={{fontWeight: 'bold'}} to="/">Get Weather</IndexLink>
+          </li>
+          <li>
+            <Link activeClassName="active" activeStyle={{fontWeight: 'bold'}} to="/about">About</Link>
+          </li>
+          <li>
+            <Link activeClassName="active" activeStyle={{fontWeight: 'bold'}} to="/examples">Examples</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <form onSubmit={this.onSearch}>
+          <ul className="menu">
+            <li>
+              <input type="search" placeholder="Enter Search"/>
+            </li>
+            <li>
+              <input className="button" type="submit" value="Get Weather"/>
+            </li>
+          </ul>
+        </form>
+      </div>
     </div>
-  );
-}
+    );
+  }
+});
 
 module.exports = Nav;
