0697ca7a35cdb404d70c4a70cb00cccb65476796
Fahd <fwahab9@gmail.com>
2017-02-01 17:51:38 -0800

    Added error modal

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -3,9 +3,9 @@ var React = require('react');
 var WeatherMessage = ({city, temp}) => {
   return (
     <div>
-      <h3 className="text-center"/>It is {temp} in {city}</h3>
+      <h3 className="text-center">It is {temp} in {city}</h3>
     </div>
   );
-};
+}
 
 module.exports = WeatherMessage;
--- webpack.config.js
+++ webpack.config.js
@@ -24,9 +24,10 @@ module.exports = {
     alias: {
       Main: 'app/components/Main.jsx',
       Nav: 'app/components/Nav.jsx',
-      Weather: 'app/components/Weather.jsx',
       About: 'app/components/About.jsx',
+      Weather: 'app/components/Weather.jsx',
       Examples: 'app/components/Examples.jsx',
+      ErrorModal: 'app/components/ErrorModal.jsx',
       WeatherForm: 'app/components/WeatherForm.jsx',
       WeatherMessage: 'app/components/WeatherMessage.jsx',
       openWeatherMap: 'app/api/openWeatherMap.jsx'
